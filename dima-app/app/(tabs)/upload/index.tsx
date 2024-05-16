3e3fd7097960aa71f971ce3bf611563f07b15512
Alexandre Boving <alexandre.boving@gmail.com>
2024-05-16 14:47:42 +0200

    Routing implementation

---
+++ dima-app/app/(tabs)/upload/index.tsx
@@ -0,0 +1,12 @@
+import { View, Text, StyleSheet } from "react-native";
+import React from "react";
+
+export default function Upload() {
+  return (
+    <View>
+      <Text>Upload</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
---
+++ dima-app/app/(tabs)/upload/learn.tsx
@@ -0,0 +1,12 @@
+import { View, Text, StyleSheet } from "react-native";
+import React from "react";
+
+export default function Learn() {
+  return (
+    <View>
+      <Text>Learn</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
